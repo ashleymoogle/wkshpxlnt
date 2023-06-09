a10399a0b319f345b89bb50b5101f194ff60bbca
Bruno Lapierre <blapierre@exolnet.com>
2023-06-09 16:01:17 -0400

    adds home option api component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,76 @@
 <script lang="ts">
-    import { defineComponent } from 'vue';
-    export default defineComponent({
-        name: 'Home',
-    })
+  import { defineComponent } from 'vue';
+  // import {Xcard} from 'Xcard';
+
+  export default defineComponent({
+    data() {
+      return {
+        title: 'Hello',
+        userId: 0,
+        completed: false,
+
+        cards: [
+          {
+            userId: 1,
+            id: 1,
+            title: "delectus aut autem",
+            completed: false
+          },
+          {
+            userId: 1,
+            id: 2,
+            title: "quis ut nam facilis et officia qui",
+            completed: false
+          }
+        ],
+      }
+    },
+
+    computed: {
+      title() {
+        return this.name;
+      },
+    },
+
+    methods: {
+      onSubmit() {
+        console.log(this.title, this.userId, this.completed)
+      }
+    },
+
+    mounted() {
+
+    },
+  })
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">
-    Home
+    {{ title }}
   </h1>
+  <ul>
+    <li v-for="card in cards" :key="card.id">
+      <p>{{ card.id }}</p>
+      <p>{{ card.title }}</p>
+      <p>{{ card.completed }}</p>
+    </li>
+  </ul>
+
+  <form class="review-form" @submit.prevent="onSubmit">
+    <p>
+      <label for="name">title:</label>
+      <input id="title" v-model="title">
+    </p>
+
+    <p>
+      <input type="checkbox" id="completed" v-model="completed">
+    </p>
+
+    <p>
+      <input type="submit" value="Submit">
+    </p>
+
+  </form>
 </template>
 
 <style scoped lang="scss">
