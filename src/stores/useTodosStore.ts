99a8bad39185ae5efbc9e86c5d144e8f5961017e
Nina Séraphin <nseraphin@exolnet.com>
2023-06-19 14:59:13 -0400

    update useTodosStore and add user type

--- src/stores/useTodosStore.ts
+++ src/stores/useTodosStore.ts
@@ -1,6 +1,6 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia'
-import { Todo } from '@/types.ts';
+import { Todo, User } from '@/types.ts';
 
 // You can name the return value of `defineStore()` anything you want,
 // but it's best to use the name of the store and surround it with `use`
@@ -8,16 +8,60 @@ import { Todo } from '@/types.ts';
 // the first argument is a unique id of the store across your application
 export const useTodosStore = defineStore('todos', () => {
     const todos = ref<Todo[]>([]);
+    const users = ref<User[]>([]);
 
-    const completedTodos = computed(() => todos.value.filter((todo: Todo) => todo.done))
+    const totalTodos = computed(() => todos.value.length);
+    const completedTodos = computed(() => todos.value.filter((todo: Todo) => todo.completed))
 
     const addTodo = (newTodo: Todo) => {
         todos.value.push(newTodo);
     }
 
+    const getToDos = async () => {
+        // @ts-ignore
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos/', {'Content-type': 'application/json; charset=UTF-8'});
+        const data = await res.json();
+        todos.value = data.slice(0, 10);
+    }
+
+    const removeToDo = async (toDoToBeRemoved:Todo) => {
+        try {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${toDoToBeRemoved.id}`, {
+                method: 'DELETE',
+            });
+
+            const index = todos.value.findIndex((item) => {
+                return item.id === toDoToBeRemoved.id
+            })
+
+            if (index !== -1) {
+                todos.value.splice(index, 1)
+            }
+        } catch(e) {
+            console.log('errors', e)
+        }
+    }
+
+    const getUsers = async () => {
+        // @ts-ignore
+        const res = await fetch('https://jsonplaceholder.typicode.com/users/', {'Content-type': 'application/json; charset=UTF-8'});
+        users.value = await res.json();
+    }
+
+    const getUserName = (todo: Todo) => {
+        const user = users.value.find((item) => item.id === todo.userId);
+        return user ? user.name : '-';
+    }
+
     return {
         addTodo,
         completedTodos,
+        getToDos,
+        getUserName,
+        getUsers,
+        removeToDo,
         todos,
+        totalTodos,
+        users,
     }
 })
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,26 @@ export interface Todo {
     title: string
     completed: boolean
 }
+export interface User {
+    id: number
+    name: string
+    username: string
+    email: string
+    address: {
+        street: string
+        suite: string
+        city: string
+        zipcode: string
+        geo: {
+            lat: string
+            lng: string
+        }
+    }
+    phone: string
+    website: string
+    company: {
+        name: string
+        catchPhrase: string
+        bs: string
+    }
+}
